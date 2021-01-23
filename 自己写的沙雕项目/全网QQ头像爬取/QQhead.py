71f2f143afcf0daf8f353ea7a8addf606c608302
WhaleFall <2734184475@qq.com>
2021-01-23 08:49:27 +0800

    更新(未完善)爬取全网QQ昵称头像。

--- 自己写的沙雕项目/全网QQ头像爬取/QQhead.py
+++ 自己写的沙雕项目/全网QQ头像爬取/QQhead.py
@@ -1,20 +1,34 @@
+'''
+Author: whalefall
+Date: 2021-01-22 14:42:59
+LastEditors: whalefall
+LastEditTime: 2021-01-23 08:48:22
+Description: 生成随机QQ号并获取他的昵称和头像,存入数据库或下载到../img文件夹(然而没什么卵用)
+'''
+
+# todo:目前没时间完善了,有时间再修改叭
+
 # coding=utf-8
+
 import requests
 import re
 import hashlib
 import sys
+import random
+import time
 import threading
-lock = threading.Lock()
+
 path = sys.path[0]
 
 header = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; MI 5X Build/OPM1.171019.019; wv) \
-	AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser\
-	/6.2 TBS/045120 Mobile Safari/537.36 V1_AND_SQ_8.2.7_1334_YYB_D QQ/8.2.7.4410 NetType\
-	/WIFI WebP/0.3.0 Pixel/1080 StatusBarHeight/72 SimpleUISwitch/0"
+    "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; MI 5X Build/OPM1.171019.019; wv) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser\
+    /6.2 TBS/045120 Mobile Safari/537.36 V1_AND_SQ_8.2.7_1334_YYB_D QQ/8.2.7.4410 NetType\
+    /WIFI WebP/0.3.0 Pixel/1080 StatusBarHeight/72 SimpleUISwitch/0"
 }
 
 
+# 获取到的图片二进制信息转md5
 def getStrAsMD5(parmStr):
     # 1、参数必须是utf8
     # 2、python3所有字符都是unicode形式，已经不存在unicode关键字
@@ -27,13 +41,24 @@ def getStrAsMD5(parmStr):
     return m.hexdigest()
 
 
-def getHead(qqID):
+# 文件名去除非法字符
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "_", title)  # 替换为下划线
+    return new_title
 
+
+def getHead():
     global name
-    url = "https://users.qzone.qq.com/fcg-bin/cgi_get_portrait.fcg?uins=" + \
+    global qqID
+    url = "https://r.qzone.qq.com/fcg-bin/cgi_get_portrait.fcg?uins=" + \
         str(qqID)
-    resp = requests.get(url, headers=header).text
-    # print(resp)
+    try:
+        resp = requests.get(url, headers=header).content.decode(encoding="gbk")
+    except Exception as e:
+        print("可能是编码错误", e)
+        return "101"
+    print(resp)
     # 匹配头像地址
     pat_h = re.compile(
         r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
@@ -48,68 +73,54 @@ def getHead(qqID):
 
 
 def download(headURL):
+    global qqID
     resp = requests.get(headURL, headers=header).content
     md5 = getStrAsMD5(resp)
-    # 空白头像 9e981dc7788599d9372c8381d776c554
+    # 空白头像 9e981dc7788599d9372c8381d776c554 6b3dc21f211fc653b3756c3392221293
+    print(md5)
     if md5 == "9e981dc7788599d9372c8381d776c554":
-        print("空白头像")
+        print(str(qqID) + " " + "空白头像")
+        pass
+    elif md5 == "6b3dc21f211fc653b3756c3392221293":
+        print(str(qqID) + " " + "空白头像")
+        pass
+    elif md5 == "de1f6b4a58422bba344f94927dd90a9b":
+        print(str(qqID) + " " + "空白头像")
         pass
     else:
         img = requests.get(headURL, headers=header).content
-        with open("{0}//img//{1}.jpg".format(path, str(qqID)+" "+name), "wb") as f:
+        name_b = validateTitle(name)
+        with open("{0}\\img\\{1}.jpg".format(path, str(qqID) + " " + name_b), "wb") as f:
             f.write(img)
-        print(str(qqID)+" "+name, "已保存")
+        with open("{0}\\qqList.txt".format(path), "a", encoding="utf-8") as l:
+            l.write("qq号：" + str(qqID) + "  " + "网名：" + name + "\n")
+        print(str(qqID) + " " + name, "已保存")
 
-# qqID="2734184475"
+
+# qqID = "2734184475"
+# url = getHead(qqID)
+# download(url)
 # qqList=["2734184475"]
+# for qqID in range(1000000000, 9999999999):
+
+def run():
+    while True:
+        try:
+            qqID = random.randint(1000000000, 9999999999)
+            url = getHead(qqID)
+            if url == "1":
+                print(qqID, "用户不存在")
+                pass
+            elif url == "101":
+                print("用户不存在")
+            else:
+                download(url)
+            print("-------------------------------")
+        except Exception as p:
+            print("大运行出现异常,停止2s", p)
+            print("-------------------------------")
+            time.sleep(2)
 
 
-def run(thread):
-    lock.acquire()
-    for id in range(1000000000, 9999999999):
-        qqID = id
-
-    url = getHead(qqID)
-    if url == "1":
-        print(thread, qqID, "用户不存在")
-        pass
-    else:
-        with open("{0}//qqList.txt".format(path), "a", encoding="utf8") as l:
-            l.write("qq号："+qqID+"  "+"网名："+name+"\n")
-        download(url)
-        print("-------------------------------")
-    lock.release()
-
-
-t1 = threading.Thread(target=run, args=("线程1",))
-t2 = threading.Thread(target=run, args=("线程2",))
-t3 = threading.Thread(target=run, args=("线程3",))
-# if id%10=="2":
-# 	t2=threading.Thread(target=run,args=(id,"线程2",))
-# if id%10=="3":
-# 	t3=threading.Thread(target=run,args=(id,"线程3",))
-# if id%10=="4":
-# 	t4=threading.Thread(target=run,args=(id,"线程4",))
-# if id%10=="5":
-# 	t5=threading.Thread(target=run,args=(id,"线程5",))
-# if id%10=="6":
-# 	t6=threading.Thread(target=run,args=(id,"线程6",))
-# if id%10=="7":
-# 	t7=threading.Thread(target=run,args=(id,"线程7",))
-# if id%10=="8":
-# 	t8=threading.Thread(target=run,args=(id,"线程8",))
-# if id%10=="9":
-# 	t9=threading.Thread(target=run,args=(id,"线程9",))
-# if id%10=="0":
-# 	t10=threading.Thread(target=run,args=(id,"线程10",))
-
-t1.start()
-# t2.start()
-# t3.start()
-# t4.start()
-# t5.start()
-# t6.start()
-# t7.start()
-# t8.start()
-# t9.start()
-# t10.start()
+for i in range(5):
+    run()
