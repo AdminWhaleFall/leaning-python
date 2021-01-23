185d08acbcaf288ebb5b5b2b5816e7d95ea651a6
WhaleFall <2734184475@qq.com>
2021-01-23 18:54:25 +0800

    结束一天的工作。

--- 自己写的沙雕项目/疫情数据.py
+++ 自己写的沙雕项目/疫情数据.py
@@ -2,13 +2,17 @@
 Author: whalefall
 Date: 2021-01-22 16:52:17
 LastEditors: whalefall
-LastEditTime: 2021-01-22 17:38:16
-Description: 爬取百度疫情数据
+LastEditTime: 2021-01-23 12:41:45
+Description: 爬取百度疫情数据,并生成各种图表
 '''
-import requests
-import re
 import json
+import re
 import sys
+
+import requests
+
+import matplotlib.pyplot as plt
+
 
 path=sys.path[0]
 url = "https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_aladin_banner"
@@ -16,7 +20,7 @@ headers = {
     "User-Agent": "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
 }
 
-resp = requests.get(url, headers=headers).text.replace(" ", "")
+resp = requests.get(url, headers=headers).text
 
 pat = re.compile(r'id="captain-config">(.*?)</script><script>')
 # 替换一些东东
@@ -27,7 +31,32 @@ result = pat.findall(resp)[0].replace("(", "").replace(")", "").replace("'",'"')
 result = json.loads(result)
 # print(result)
 
-with open(r"{}//疫情数据.json".format(path),"w",encoding="utf8") as f:
-    f.write(str(result))
-    print("写入成功!")
+update_t=result["component"][0]["mapLastUpdatedTime"] # 数据更新时间
+print(update_t)
+# 国内数据列表
+areaList=[] #储存地区
+confirmedList=[] #储存累计确诊
+caseList=result["component"][0]["caseList"]
+
+# Todo:我想让他们从大到小排列诶 怎么实现
+for provinces in caseList:
+    area=provinces["area"] #地区
+    confirmed=provinces["confirmed"] # 累计确诊
+    # print(area,confirmed)
+    areaList.append(area)
+    confirmedList.append(confirmed)
+
+# 数据可视化部分
+fig=plt.figure()
+ax=fig.add_subplot(111)
+# ax.set(font="SimSun")
+#解决中文显示问题
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+ax.bar(areaList,confirmedList,color="lightblue",align="center")
+plt.show()
+
+
+
 
