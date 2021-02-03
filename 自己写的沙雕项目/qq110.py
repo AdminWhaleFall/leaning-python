a588df70530eae0e63c34d88f8c6bf239cd81e64
WhaleFall <2734184475@qq.com>
2021-02-04 00:21:31 +0800

    添加颜色

--- 自己写的沙雕项目/qq110.py
+++ 自己写的沙雕项目/qq110.py
@@ -2,7 +2,7 @@
 Author: whalefall
 Date: 2021-02-03 00:02:28
 LastEditors: whalefall
-LastEditTime: 2021-02-03 20:30:37
+LastEditTime: 2021-02-03 21:22:58
 Description: 多线程请求`qq110.net`信誉查询平台 认真复习了一下多线程知识点
 '''
 import requests
@@ -11,7 +11,7 @@ import threading  # 多线程
 import random
 
 
-def qqID(content,qqID):
+def qqID(content, qqID):
     headers = {
         'Connection': 'keep-alive',
         'Pragma': 'no-cache',
@@ -32,16 +32,21 @@ def qqID(content,qqID):
     try:
         response = requests.post(
             'http://qq110.net/', headers=headers, data=data, verify=False)
-        print(content,response.url, qqID)
+        if str(response.status_code) == "200":
+            print(" {} \033[1;30;42m{} {} \033[0m".format(
+                content, response.url, response.status_code))
+        else:
+            print(content, response.url, response.status_code)
+
     except ConnectionError:
         print("\033[1;30;41m[Error]服务器连接超时!\033[0m")
     except Exception as e:
-        print("\033[1;30;41m[Error]服务器未知错误!\033[0m",e)
+        print("\033[1;30;41m[Error]服务器未知错误!\033[0m", e)
 
 
 def main(th):
     while True:
-        qq = random.randint(1000000000,9999999999)
+        qq = random.randint(1000000000, 9999999999)
         qqID("\033[1;30;44m 线程{} \033[0m".format(str(th)), qq)
 
 
@@ -49,13 +54,13 @@ def main(th):
 if __name__ == '__main__':
     threads = []
     thread_name = []
-    for i in range(1,257):
+    for i in range(1, 257):
         thread_name.append(i)
-    
+
     for name in thread_name:
         t = threading.Thread(target=main, args=(name,))
         t.start()
         threads.append(t)
-        
+
     for thread in threads:
         thread.join()
